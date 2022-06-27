18f6c54d4bc7a7af01fb32f0fe31c715712fece9
vsumm <sumis2140@gmail.com>
2022-06-27 17:44:38 +0530

    Updates

---
+++ precache-manifest.57a0a3e2d22854d35dbaa4240de4414f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "47aa7de6adc1c518ccd2",
+    "url": "/express_bees/static/css/main.77b2d8ff.chunk.css"
+  },
+  {
+    "revision": "47aa7de6adc1c518ccd2",
+    "url": "/express_bees/static/js/main.47aa7de6.chunk.js"
+  },
+  {
+    "revision": "ff02c862f48e157de30b",
+    "url": "/express_bees/static/css/1.df56fcec.chunk.css"
+  },
+  {
+    "revision": "ff02c862f48e157de30b",
+    "url": "/express_bees/static/js/1.ff02c862.chunk.js"
+  },
+  {
+    "revision": "8d285aca452769b60fbf",
+    "url": "/express_bees/static/js/2.8d285aca.chunk.js"
+  },
+  {
+    "revision": "9dad515986479f985318",
+    "url": "/express_bees/static/js/3.9dad5159.chunk.js"
+  },
+  {
+    "revision": "472effa229dcd6016ce8",
+    "url": "/express_bees/static/js/4.472effa2.chunk.js"
+  },
+  {
+    "revision": "7522b13ae320724585ac",
+    "url": "/express_bees/static/js/runtime~main.7522b13a.js"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/express_bees/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/express_bees/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "241903cf740e11c47a5fd0b9133159cc",
+    "url": "/express_bees/static/media/logo.241903cf.png"
+  },
+  {
+    "revision": "22c2d0c8a862dfaa43a271092986386c",
+    "url": "/express_bees/static/media/profile.22c2d0c8.jpg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/express_bees/static/media/svg-3.397c455a.svg"
+  },
+  {
+    "revision": "65cc8e9693e717e987b8644146ccd7a1",
+    "url": "/express_bees/index.html"
+  }
+];
