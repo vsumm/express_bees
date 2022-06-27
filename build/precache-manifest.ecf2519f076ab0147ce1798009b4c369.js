81b0056f5bbd4e468ccd0f56448f69a6aa9ae230
vsumm <sumis2140@gmail.com>
2022-06-27 17:26:19 +0530

    first commit

---
+++ build/precache-manifest.ecf2519f076ab0147ce1798009b4c369.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a6cb294e7e00f36fff5",
+    "url": "/express_bees/static/css/main.77b2d8ff.chunk.css"
+  },
+  {
+    "revision": "9a6cb294e7e00f36fff5",
+    "url": "/express_bees/static/js/main.9a6cb294.chunk.js"
+  },
+  {
+    "revision": "cdae9b11abce867f050a",
+    "url": "/express_bees/static/css/1.df56fcec.chunk.css"
+  },
+  {
+    "revision": "cdae9b11abce867f050a",
+    "url": "/express_bees/static/js/1.cdae9b11.chunk.js"
+  },
+  {
+    "revision": "16db571ff38d6bbf2d89",
+    "url": "/express_bees/static/js/2.16db571f.chunk.js"
+  },
+  {
+    "revision": "9dad515986479f985318",
+    "url": "/express_bees/static/js/3.9dad5159.chunk.js"
+  },
+  {
+    "revision": "472effa229dcd6016ce8",
+    "url": "/express_bees/static/js/4.472effa2.chunk.js"
+  },
+  {
+    "revision": "b4ba7a2cadb0682786d7",
+    "url": "/express_bees/static/js/runtime~main.b4ba7a2c.js"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/express_bees/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/express_bees/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "241903cf740e11c47a5fd0b9133159cc",
+    "url": "/express_bees/static/media/logo.241903cf.png"
+  },
+  {
+    "revision": "22c2d0c8a862dfaa43a271092986386c",
+    "url": "/express_bees/static/media/profile.22c2d0c8.jpg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/express_bees/static/media/svg-3.397c455a.svg"
+  },
+  {
+    "revision": "1ca49bbd7b3a8c6d22751170c18b9215",
+    "url": "/express_bees/index.html"
+  }
+];
